f9831f9fcde6beb97cda79747a008a88256ae181
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-01-19 01:42:20 +0600

    feat: add Projects section with category filtering and carousel display

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-
-import React from 'react'
-import Navbar from './components/layout/Navbar'
-import Hero from './components/sections/Hero'
-import About from './components/sections/About'
-import Skills from './components/sections/Skills'
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Hero from "./components/sections/Hero";
+import About from "./components/sections/About";
+import Skills from "./components/sections/Skills";
+import Projects from "./components/sections/Projects";
 function App() {
-
-
   return (
-  <div className="min-h-screen bg-black pb-[100vh]">
-    <Navbar />
-    <main>
-      <Hero/>
-      <About/>
-      <Skills/>
-    </main>
+    <div className="min-h-screen bg-black pb-[100vh]">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+      </main>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
